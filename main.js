68d5036f9c4701af075b42f850ef0174bffa06ae
namborghini <121301710+n4mborgh1n1@users.noreply.github.com>
2023-07-01 05:00:02 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Öffnen/Schließen des Dropdown-Menüs bei Klick auf den Button
-document.getElementsByClassName("dropbtn")[0].addEventListener("click", function() {
+document.getElementsByClassName("dropBtn")[0].addEventListener("click", function() {
     this.classList.toggle("active");
     var dropdownContent = this.nextElementSibling;
     if (dropdownContent.style.display === "block") {
@@ -15,8 +15,25 @@ document.getElementsByClassName("dropbtn")[0].addEventListener("click", function
     if (!dropdown.contains(event.target)) {
       var dropdownContent = dropdown.getElementsByClassName("dropdown-content")[0];
       dropdownContent.style.display = "none";
-      var dropbtn = dropdown.getElementsByClassName("dropbtn")[0];
-      dropbtn.classList.remove("active");
+      var dropBtn = dropdown.getElementsByClassName("dropBtn")[0];
+      dropBtn.classList.remove("active");
     }
   });
+
+  function toggleColorTheme() {
+    var body = document.body;
+    body.classList.toggle("colorTheme");
+  
+    var mainColor = getComputedStyle(body).getPropertyValue("--main-color");
+    var secColor = getComputedStyle(body).getPropertyValue("--sec-color");
+  
+    if (body.classList.contains("colorTheme")) {
+      body.style.setProperty("--main-color", "#cfcfcf"); 
+      body.style.setProperty("--sec-color", "#0c0013"); 
+    } else {
+      body.style.setProperty("--main-color", "#0c0013"); 
+      body.style.setProperty("--sec-color", "#cfcfcf"); 
+    }
+  }
+  
   
