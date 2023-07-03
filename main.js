d4f8a01934aaf6c598d3e154d57a1165f70cff35
namborghini <121301710+n4mborgh1n1@users.noreply.github.com>
2023-07-03 21:06:53 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -20,39 +20,49 @@ document.getElementsByClassName("dropBtn")[0].addEventListener("click", function
     }
   });
 
-  /* Dunkelmodus/Hellmodus */
-
-  function toggleColorTheme() {
-    var body = document.body;
-    body.classList.toggle("colorTheme");
-  
-    var mainColor = getComputedStyle(body).getPropertyValue("--main-color");
-    var secColor = getComputedStyle(body).getPropertyValue("--sec-color");
-    var linkColor = getComputedStyle(body).getPropertyValue("--link-color");
-    
-    if (body.classList.contains("colorTheme")) { /* Press Button to switch to light mode */
-
-      body.style.setProperty("--main-color", "#cfcfcf"); /* grey */
-      body.style.setProperty("--accent-color", "#0c0013"); /* dark purple */
-      body.style.setProperty("--link-color", "rgb(30, 30, 30)"); /* dark grey */
-
-      body.style.setProperty("--greeting", "rgba(25, 25, 25, 0.144"); /* transparent grey */
-      body.style.setProperty("--section-color", "rgb(30, 30, 30)"); /* dark grey */
-      body.style.setProperty("--hover", "rgba(34, 34, 34, 0.075)"); /* transparent grey */
-
-    
-    } else { 
-      body.style.setProperty("--main-color", "#0c0013"); /* dark purple */
-      body.style.setProperty("--accent-color", "#cfcfcf"); /* grey */
-      body.style.setProperty("--link-color", "rgb(224, 224, 0)"); /* yellow */
-
-      body.style.setProperty("--greeting", "rgba(255, 255, 255, 0.045)"); /* transparent grey */
-      body.style.setProperty("--section-color", "rgb(191, 191, 191)");
-      body.style.setProperty("--hover", "rgba(255, 255, 255, 0.075)"); /* transparent grey */
-    }
+  // Überprüfen, ob ein gespeicherter Farbmodus vorhanden ist
+var savedColorMode = localStorage.getItem("colorMode");
+if (savedColorMode) {
+  // Überprüfen, ob der gespeicherte Farbmodus "dark" ist
+  if (savedColorMode === "dark") {
+    // Aktivieren des Dunkelmodus
+    toggleColorTheme();
   }
+}
 
-  gsap.from('div, img', {opacity: 0, duration: 1, y: -50})
+function toggleColorTheme() {
+  var body = document.body;
+  body.classList.toggle("colorTheme");
+
+  var mainColor = getComputedStyle(body).getPropertyValue("--main-color");
+  var secColor = getComputedStyle(body).getPropertyValue("--sec-color");
+  var linkColor = getComputedStyle(body).getPropertyValue("--link-color");
+
+  if (body.classList.contains("colorTheme")) {
+    // Dunkelmodus 
+    body.style.setProperty("--main-color", "#cfcfcf");
+    body.style.setProperty("--accent-color", "#0c0013");
+    body.style.setProperty("--link-color", "rgb(30, 30, 30)");
+    body.style.setProperty("--greeting", "rgba(25, 25, 25, 0.144");
+    body.style.setProperty("--section-color", "rgb(30, 30, 30)");
+    body.style.setProperty("--hover", "rgba(34, 34, 34, 0.075)");
+    // Farbmodus im localStorage speichern
+    localStorage.setItem("colorMode", "dark");
+  } else {
+    // Hellmodus
+    body.style.setProperty("--main-color", "#0c0013");
+    body.style.setProperty("--accent-color", "#cfcfcf");
+    body.style.setProperty("--link-color", "rgb(224, 224, 0)");
+    body.style.setProperty("--greeting", "rgba(255, 255, 255, 0.045)");
+    body.style.setProperty("--section-color", "rgb(191, 191, 191)");
+    body.style.setProperty("--hover", "rgba(255, 255, 255, 0.075)");
+    // Farbmodus im localStorage speichern
+    localStorage.setItem("colorMode", "light");
+  }
+}
+
+
+  gsap.from('div, img', {opacity: 0, duration: 0.5, y: -40, stagger: 0.1})
 
 
   
